a08e2c7fa9b8242f63a614cdbd4141eb2499a69f
Ruchita Patil <71245429+ruchspatil@users.noreply.github.com>
2023-12-25 20:41:40 +0530

    Update views.py

--- News Aggregator/news/views.py
+++ News Aggregator/news/views.py
@@ -2,8 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
 
-# GEtting news from Times of India
-
 toi_r = requests.get("https://timesofindia.indiatimes.com/briefs")
 toi_soup = BeautifulSoup(toi_r.content, 'html5lib')
 
@@ -18,17 +16,5 @@ for th in toi_headings:
 
 
 
-#Getting news from Hindustan times
-
-ht_r = requests.get("https://www.hindustantimes.com/india-news/")
-ht_soup = BeautifulSoup(ht_r.content, 'html5lib')
-ht_headings = ht_soup.findAll("div", {"class": "headingfour"})
-ht_headings = ht_headings[2:]
-ht_news = []
-
-for hth in ht_headings:
-    ht_news.append(hth.text)
-
-
 def index(req):
-    return render(req, 'index.html', {'toi_news':toi_news, 'ht_news': ht_news})
+    return render(req, 'index.html', {'toi_news':toi_news})
